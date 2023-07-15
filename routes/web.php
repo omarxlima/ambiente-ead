2db9c3abbc18f9ff28114d3c395966d839389d87
marxlima <marxmaciel@gmail.com>
2023-07-15 02:06:40 -0300

    create template

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\{
+    AdminController
+};
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::prefix('admin')->group(function(){
+    Route::get('/', [AdminController::class, 'index'])->name('admin.home');
+});
 
 Route::get('/', function () {
     return view('welcome');
